5dfb6f7e3ed85e9eaeca9368ee5d833b449e0d11
se0v <seovulf@gmail.com>
2025-03-12 02:51:16 +0300

    pre-url

--- lib/pages/note_page.dart
+++ lib/pages/note_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:netbrains/components/my_note_tile.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../components/my_drawer.dart';
 import '../models/note.dart';
@@ -78,17 +80,92 @@ class _NotePageState extends State<NotePage> {
       context: context,
       builder: (context) => AlertDialog(
         title: const Text("Добавить заметку"),
-        content: TextField(controller: _noteController),
+        content: TextField(
+          controller: _noteController,
+          keyboardType: TextInputType.multiline,
+          maxLines: null,
+          decoration: const InputDecoration(
+            hintText: "Введите заметку (ссылки определяются автоматически)",
+          ),
+        ),
         actions: [
           TextButton(
             onPressed: () async {
-              await databaseProvider.createNote(_noteController.text);
+              String noteText = _noteController.text;
               Navigator.pop(context);
+              await databaseProvider.createNote(noteText);
+              _showNoteWithLinks(noteText);
             },
             child: const Text("Сохранить"),
           ),
         ],
       ),
     );
+  }
+
+  /// Показывает диалог с обработанными ссылками
+  void _showNoteWithLinks(String noteText) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Просмотр заметки"),
+        content: SelectableText.rich(
+          _parseTextWithLinks(noteText),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Закрыть"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// Парсит текст, определяя ссылки и превращая их в кликабельные `TextSpan`
+  TextSpan _parseTextWithLinks(String text) {
+    final RegExp linkRegex = RegExp(
+      r'(https?:\/\/[^\s]+)', // Регулярное выражение для поиска ссылок
+      caseSensitive: false,
+    );
+
+    List<TextSpan> spans = [];
+    int lastMatchEnd = 0;
+
+    final matches = linkRegex.allMatches(text);
+    for (final match in matches) {
+      if (match.start > lastMatchEnd) {
+        spans.add(TextSpan(text: text.substring(lastMatchEnd, match.start)));
+      }
+
+      final String url = match.group(0)!;
+      spans.add(
+        TextSpan(
+          text: url,
+          style: const TextStyle(
+              color: Colors.blue, decoration: TextDecoration.underline),
+          recognizer: TapGestureRecognizer()..onTap = () => _openUrl(url),
+        ),
+      );
+
+      lastMatchEnd = match.end;
+    }
+
+    if (lastMatchEnd < text.length) {
+      spans.add(TextSpan(text: text.substring(lastMatchEnd)));
+    }
+
+    return TextSpan(
+        style: const TextStyle(color: Colors.black), children: spans);
+  }
+
+  /// Открывает URL в браузере
+  void _openUrl(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      print("Не удалось открыть ссылку: $url");
+    }
   }
 }
